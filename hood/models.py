f94f1ebacf1fa8305df0e3a784410a45906efb56
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-26 21:55:42 +0300

    Create new business model

--- hood/models.py
+++ hood/models.py
@@ -7,18 +7,16 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    logo = models.ImageField(upload_to='images/', default='hoodlogo.png')
+    hood_logo = models.ImageField(upload_to='images/')
     description = models.TextField()
     def __str__(self):
         return f'{self.name} hood'
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
-    neighbourhood = models.ForeignKey(NeighbourHood, null=True, on_delete=models.CASCADE, related_name='people')
-
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
     def __str__(self):
         return f'{self.user.username} profile'
     @receiver(post_save, sender=User)
@@ -28,3 +26,15 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name} Business'
+
