250f6a110d72b9e9dbaf4aa4e375694ea8315451
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-27 21:46:12 +0300

    push to heroku

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
@@ -10,5 +11,8 @@ urlpatterns = [
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
     path('join_hood/<id>', views.join_hood, name='join-hood'),
     path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
-    path('single_hood/<hood_id>', views.single_hood, name='single-hood')
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,13 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, BusinessForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from .models import NeighbourHood, Profile, Business
-from .forms import UpdateProfileForm, NeighbourHoodForm
+from .models import NeighbourHood, Profile, Business, Post
+from .forms import UpdateProfileForm, NeighbourHoodForm, PostForm
 from django.contrib.auth.models import User
+
+
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -21,12 +26,17 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
 def hoods(request):
     all_hoods = NeighbourHood.objects.all()
+    all_hoods = all_hoods[::-1]
     params = {
         'all_hoods': all_hoods,
     }
     return render(request, 'all_hoods.html', params)
+
+
 def create_hood(request):
     if request.method == 'POST':
         form = NeighbourHoodForm(request.POST, request.FILES)
@@ -38,9 +48,13 @@ def create_hood(request):
     else:
         form = NeighbourHoodForm()
     return render(request, 'newhood.html', {'form': form})
+
+
 def single_hood(request, hood_id):
     hood = NeighbourHood.objects.get(id=hood_id)
     business = Business.objects.filter(neighbourhood=hood)
+    posts = Post.objects.filter(hood=hood)
+    posts = posts[::-1]
     if request.method == 'POST':
         form = BusinessForm(request.POST)
         if form.is_valid():
@@ -54,21 +68,51 @@ def single_hood(request, hood_id):
     params = {
         'hood': hood,
         'business': business,
-        'form': form
+        'form': form,
+        'posts': posts
     }
     return render(request, 'single_hood.html', params)
+
+
+def hood_members(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    members = Profile.objects.filter(neighbourhood=hood)
+    return render(request, 'members.html', {'members': members})
+
+
+def create_post(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {'form': form})
+
+
 def join_hood(request, id):
     neighbourhood = get_object_or_404(NeighbourHood, id=id)
     request.user.profile.neighbourhood = neighbourhood
     request.user.profile.save()
     return redirect('hood')
+
+
 def leave_hood(request, id):
     hood = get_object_or_404(NeighbourHood, id=id)
     request.user.profile.neighbourhood = None
     request.user.profile.save()
     return redirect('hood')
+
+
 def profile(request, username):
     return render(request, 'profile.html')
+
+
 def edit_profile(request, username):
     user = User.objects.get(username=username)
     if request.method == 'POST':
@@ -79,3 +123,19 @@ def edit_profile(request, username):
     else:
         form = UpdateProfileForm(instance=request.user.profile)
     return render(request, 'editprofile.html', {'form': form})
+
+
+def search_business(request):
+    if request.method == 'GET':
+        name = request.GET.get("title")
+        results = Business.objects.filter(name__icontains=name).all()
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, "results.html")
